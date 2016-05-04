d9ef9e69032a9074f33a2c1f97033b950e27cff6
Donny Davis <donnywdavis@icloud.com>
2016-05-04 14:54:42 -0400

    Add some comments to remember what we are doing

--- Atbash/Atbash/ViewController.m
+++ Atbash/Atbash/ViewController.m
@@ -55,24 +55,35 @@
     NSRange rangeOfLetter;
     NSString *letter = @"";
     
+    // spin through each word in the string to encrypt
     for (NSString *word in wordsArray) {
         int index = 0;
+        // spin through each character in the word to encrypt it
         while (index < word.length) {
+            // get the specific letter for this iteration
             letter = [NSString stringWithFormat:@"%c", [word characterAtIndex:index]];
+            // check if the letter is lower case
             if ([letter rangeOfCharacterFromSet:[self.lowerCaseSet invertedSet]].location == NSNotFound) {
+                // get the location of the letter in the alphabet string
                 rangeOfLetter = [self.lowerCase rangeOfString:letter];
+                // append our new string with the corresponding letter from the reverse string
                 [newString setString:[newString stringByAppendingString:[NSString stringWithFormat:@"%c", (char)[self.lowerCaseReverse characterAtIndex:rangeOfLetter.location]]]];
                 
+                // check if the letter is upper case
             } else if ([letter rangeOfCharacterFromSet:[self.upperCaseSet invertedSet]].location == NSNotFound) {
+                // get the location of the letter in the alphabet string
                 rangeOfLetter = [self.upperCase rangeOfString:letter];
+                // append our new string with the corresponding letter from the reverse string
                 [newString setString:[newString stringByAppendingString:[NSString stringWithFormat:@"%c", (char)[self.upperCaseReverse characterAtIndex:rangeOfLetter.location]]]];
                 
+                // if it's not a letter then just append it to the string
             } else {
                 [newString setString:[newString stringByAppendingString:letter]];
             }
             index += 1;
         }
         
+        // if we have more than one word then add a space before starting the next word
         if (wordsArray.count > 1) {
             [newString setString:[newString stringByAppendingString:@" "]];
         }
@@ -87,24 +98,34 @@
     NSRange rangeOfLetter;
     NSString *letter = @"";
     
+    // spin through each word in the string to decrypt
     for (NSString *word in wordsArray) {
         int index = 0;
+        // spin through each character in the word to decrypt it
         while (index < word.length) {
             letter = [NSString stringWithFormat:@"%c", [word characterAtIndex:index]];
+            // check if the letter is lower case
             if ([letter rangeOfCharacterFromSet:[self.lowerCaseSet invertedSet]].location == NSNotFound) {
+                // get the location of the letter in the alphabet string
                 rangeOfLetter = [self.lowerCaseReverse rangeOfString:letter];
+                // append our new string with the corresponding letter from the reverse string
                 [newString setString:[newString stringByAppendingString:[NSString stringWithFormat:@"%c", (char)[self.lowerCase characterAtIndex:rangeOfLetter.location]]]];
                 
+                // check if the letter is upper case
             } else if ([letter rangeOfCharacterFromSet:[self.upperCaseSet invertedSet]].location == NSNotFound) {
+                // get the location of the letter in the alphabet string
                 rangeOfLetter = [self.upperCaseReverse rangeOfString:letter];
+                // append our new string with the corresponding letter from the reverse string
                 [newString setString:[newString stringByAppendingString:[NSString stringWithFormat:@"%c", (char)[self.upperCase characterAtIndex:rangeOfLetter.location]]]];
                 
+                // if it's not a letter then just append it to the string
             } else {
                 [newString setString:[newString stringByAppendingString:letter]];
             }
             index += 1;
         }
         
+        // if we have more than one word then add a space before starting the next word
         if (wordsArray.count > 1) {
             [newString setString:[newString stringByAppendingString:@" "]];
         }
