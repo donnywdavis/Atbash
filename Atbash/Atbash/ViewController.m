813c42bb625964496e940d7c3e9027c5609d2915
Donny Davis <donnywdavis@icloud.com>
2016-05-04 14:13:11 -0400

    Take a string entered and perform an Atbash cipher on it to encrypt it

--- Atbash/Atbash/ViewController.m
+++ Atbash/Atbash/ViewController.m
@@ -36,11 +36,26 @@
     NSMutableString *newString = [[NSMutableString alloc] init];
     NSString *forward = @"abcdefghijklmnopqrstuvwxyz";
     NSString *reverse = @"zyxwvutsrqponmlkjihgfedcba";
+    NSCharacterSet *letterSet = [NSCharacterSet characterSetWithCharactersInString:forward];
+    NSArray *wordsArray = [stringToConvert componentsSeparatedByString:@" "];
+    NSRange rangeOfLetter;
+    NSString *letter = @"";
     
-    int index = (int)stringToConvert.length - 1;
-    while (index >= 0) {
-        [newString setString:[newString stringByAppendingString:[NSString stringWithFormat:@"%c", (char)[stringToConvert characterAtIndex:index]]]];
-        index -= 1;
+    for (NSString *word in wordsArray) {
+        int index = 0;
+        while (index < word.length) {
+            letter = [NSString stringWithFormat:@"%c", [word characterAtIndex:index]];
+            rangeOfLetter = [forward rangeOfString:letter];
+            if ([letter rangeOfCharacterFromSet:[letterSet invertedSet]].location == NSNotFound) {
+                [newString setString:[newString stringByAppendingString:[NSString stringWithFormat:@"%c", (char)[reverse characterAtIndex:rangeOfLetter.location]]]];
+            } else {
+                [newString setString:[newString stringByAppendingString:letter]];
+            }
+            index += 1;
+        }
+        if (wordsArray.count > 1) {
+            [newString setString:[newString stringByAppendingString:@" "]];
+        }
     }
     
     return [newString copy];
