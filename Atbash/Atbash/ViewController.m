2b12e921ecf939b1af0406b4dfe8897b0c757698
Donny Davis <donnywdavis@icloud.com>
2016-05-04 12:20:48 -0400

    Initial logic that will reverse the string entered

--- Atbash/Atbash/ViewController.m
+++ Atbash/Atbash/ViewController.m
@@ -8,10 +8,12 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UITextFieldDelegate>
 
 @property (weak, nonatomic) IBOutlet UITextField *stringTextField;
 @property (weak, nonatomic) IBOutlet UILabel *outputLabel;
+
+- (NSString *)performAtbash:(NSString *)stringToConvert;
 
 @end
 
@@ -20,6 +22,9 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    self.outputLabel.text = @"";
+    [self.stringTextField becomeFirstResponder];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -27,7 +32,37 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (NSString *)performAtbash:(NSString *)stringToConvert {
+    NSMutableString *newString = [[NSMutableString alloc] init];
+    NSString *forward = @"abcdefghijklmnopqrstuvwxyz";
+    NSString *reverse = @"zyxwvutsrqponmlkjihgfedcba";
+    
+    int index = (int)stringToConvert.length - 1;
+    while (index >= 0) {
+        [newString setString:[newString stringByAppendingString:[NSString stringWithFormat:@"%c", (char)[stringToConvert characterAtIndex:index]]]];
+        index -= 1;
+    }
+    
+    return [newString copy];
+}
+
+#pragma mark - Button Actions 
+
 - (IBAction)atbashAction:(UIButton *)sender {
+    if (![self.stringTextField.text isEqualToString:@""]) {
+        self.outputLabel.text = [self performAtbash:self.stringTextField.text];
+    } else {
+        UIAlertController *alertController = [UIAlertController alertControllerWithTitle:@"Error" message:@"Must enter string to convert" preferredStyle:UIAlertControllerStyleAlert];
+        UIAlertAction *okButton = [UIAlertAction actionWithTitle:@"Ok" style:UIAlertActionStyleDefault handler:nil];
+        [alertController addAction:okButton];
+        [self presentViewController:alertController animated:YES completion:nil];
+    }
+}
+
+#pragma mark - Text Field Delegates
+
+- (void)textFieldDidBeginEditing:(UITextField *)textField {
+    self.outputLabel.text = @"";
 }
 
 @end
